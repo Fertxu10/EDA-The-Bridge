67b27b438f8f8efa756f4962ba41f3517362ea34
Fertxu10 <fertxusan15@gmail.com>
2025-04-07 11:05:11 +0200

    Reorganizacion carpetas

--- src/Funciones/Funciones.py
+++ src/Funciones/Funciones.py
@@ -30,3 +30,29 @@ def imputar_offsides(df, equipo_col, offside_col):
         df_resultado.loc[mask_equipo & df[offside_col].isnull(), offside_col] = media_equipo
 
     return df_resultado
+
+
+# Determinar quién metió el primer gol basado en los minutos
+def quien_anoto_primero(row):
+    h = row['minuto_gol_home']
+    a = row['minuto_gol_away']
+    
+    if pd.isna(h) and pd.isna(a):
+        return 'ninguno'
+    elif pd.isna(h):
+        return 'away'
+    elif pd.isna(a):
+        return 'home'
+    else:
+        return 'home' if h < a else 'away' if a < h else 'simultaneo'
+    
+
+def extraer_minuto(texto):
+    if pd.isna(texto) or texto.strip() == '':
+        return np.nan
+    try:
+        # Usa regex para extraer todos los numeritos del string
+        minutos = [int(s) for s in re.findall(r'\d+', texto)]
+        return min(minutos) if minutos else np.nan
+    except:
+        return np.nan
